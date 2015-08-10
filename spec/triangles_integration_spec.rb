7eb6f3be051016b7ee8870193c211b15ea94cc6e
Evan Clough & Janice Laset-Parkerson <student@epicodus.com>
2015-08-10 11:04:01 -0700

    Write integration spec to test triangle path through Sinatra app.

---
+++ spec/triangles_integration_spec.rb
@@ -0,0 +1,16 @@
+require('capybara/rspec')
+require('launchy')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe("the triangle path", {:type => :feature}) do
+  it("tests user input of sides of triangle") do
+    visit('/')
+    fill_in('A', :with => '2')
+    fill_in('B', :with => '3')
+    fill_in('C', :with => '4')
+    click_button('Trianglize!')
+    expect(page).to have_content('Your inputs form a scalene triangle.')
+  end
+end
