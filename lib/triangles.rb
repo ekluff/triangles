a2188294454d4a95e98fca178b54ea69db1a7ab5
Evan Clough & Janice Laset-Parkerson <student@epicodus.com>
2015-08-10 09:52:16 -0700

    Write method triangles?. It will return false when the greatest side length is greater than the sum of the two shorter side lengths.

---
+++ lib/triangles.rb
@@ -0,0 +1,11 @@
+class Triangle
+  define_method(:initialize) do |a, b, c|
+    @triangle_sides = [a, b, c].sort
+  end
+
+  define_method(:triangles?) do
+    if @triangle_sides[2] > @triangle_sides[0] + @triangle_sides[1]
+      false
+    end
+  end
+end
